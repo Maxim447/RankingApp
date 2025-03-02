87d9a8c02e3e94ca726e52dad596066285041106
Maxim447 <maximbas777@gmail.com>
2025-03-02 23:07:34 +0300

    feat: Добавлен рест на получение информации о платеже

--- src/main/java/ru/hse/rankingapp/controller/PaymentController.java
+++ src/main/java/ru/hse/rankingapp/controller/PaymentController.java
@@ -4,11 +4,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.hse.rankingapp.dto.payment.PaymentCreateDto;
+import ru.hse.rankingapp.dto.payment.PaymentInfoDto;
 import ru.hse.rankingapp.dto.payment.PaymentResultDto;
 import ru.hse.rankingapp.service.PaymentService;
 
@@ -30,5 +33,14 @@ public class PaymentController {
     @Operation(summary = "Создать платеж")
     public PaymentResultDto createPayment(@RequestBody @Valid PaymentCreateDto paymentCreateDto) {
         return paymentService.createPayment(paymentCreateDto);
+    }
+
+    /**
+     * Получить информацию о платеже.
+     */
+    @GetMapping("/{paymentId}")
+    @Operation(summary = "Получить информацию о платеже")
+    public PaymentInfoDto getPaymentInfo(@PathVariable(value = "paymentId") String paymentId) {
+        return paymentService.getPaymentInfo(paymentId);
     }
 }
